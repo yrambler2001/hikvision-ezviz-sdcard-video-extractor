1d8c2049fd95daeacec157cc14ea5abd4648268d
Yurii Synyshyn <yrambler2001@gmail.com>
2025-07-26 03:28:11 +0200

    completely refactored

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const a = require('./lib')
-const aa = new a('/Users/yrambler2001/Desktop/aaaaa')
+const aa = new a('/Users/yrambler2001/Desktop/unt/sdcard')
+const aaa = aa.processSegments();
+aa.extractAllSegments('./ttmmpp')
--- lib.js
+++ lib.js
@@ -1,203 +1,160 @@
-// libhikvision.js
 const fs = require('fs');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
-const { DateTime } = require('luxon');
 const { execSync } = require('child_process');
 
 class LibHikvision {
-  constructor(cameradir, asktype = 'video') {
-    this.cameradir = cameradir;
-    this.asktype = asktype;
+  constructor(cameraDir) {
+    this.cameraDir = cameraDir;
 
-    this.header_len = 1280;
+    this.headerBufferLength = 1280;
     this.file_len = 32;
-    this.segment_len = 80;
-    this.maxSegments = 256;
-    this.video_len = 4096;
+    this.segmentBufferLength = 80;
+    this.maxSegmentsInSourceFile = 256;
+    // this.video_len = 4096;
 
     this.indexType = null;
     this.segments = [];
 
-    this.getIndexPath();
-    this.info = this.getNASInfo();
+    this.indexPath = path.join(this.cameraDir, 'index00.bin'); // index01.bin has the same segments
     this.header = this.getFileHeader();
   }
 
-  getIndexPath(indexFileNum = 0) {
-    if (["video", "mp4"].includes(this.asktype)) {
-      this.indexFile = "index00.bin";
-    } else {
-      this.indexFile = "index00p.bin";
-    }
-
-    let filename = path.join(this.cameradir, `datadir${indexFileNum}`, this.indexFile);
-    if (!fs.existsSync(filename)) {
-      this.indexType = "sqlite";
-      this.indexFile = "record_db_index00";
-      filename = path.join(this.cameradir, `datadir${indexFileNum}`, this.indexFile);
-      if (!fs.existsSync(filename)) {
-        throw new Error("Can't find indexes...");
-      }
-    } else {
-      this.indexType = "bin";
-    }
-
-    return filename;
-  }
-
-  getNASInfo() {
-    const info_keys = ["serialNumber", "MACAddr", "byRes", "f_bsize", "f_blocks", "DataDirs"];
-    const buffer = fs.readFileSync(path.join(this.cameradir, "info.bin"));
-    const unpacked = [
-      buffer.slice(0, 48).toString("utf-8").replace(/\0/g, ""),
-      buffer.readUInt32LE(48),
-      buffer.readUInt8(52),
-      buffer.readUInt32LE(53),
-      buffer.readUInt32LE(57),
-      buffer.readUInt32LE(61),
-    ];
-
-    return Object.fromEntries(info_keys.map((k, i) => [k, unpacked[i]]));
-  }
-
   getFileHeader() {
-    let header = null;
-    for (let indexFileNum = 0; indexFileNum < this.info.DataDirs; indexFileNum++) {
-      const fileName = this.getIndexPath(indexFileNum);
-      const buffer = fs.readFileSync(fileName);
-      header = {
-        modifyTimes: Number(buffer.readBigUInt64LE(0)),
-        version: buffer.readUInt32LE(8),
-        avFiles: buffer.readUInt32LE(12),
-        nextFileRecNo: buffer.readUInt32LE(16),
-        lastFileRecNo: buffer.readUInt32LE(20),
-        curFileRec: buffer.readUInt32LE(24),
-        unknown: buffer.slice(28, 1204),
-        checksum: buffer.readUInt32LE(1276),
-      };
-    }
-    return header;
+    const buffer = fs.readFileSync(this.indexPath);
+
+    let offset = 0;
+    const modifyTimes = buffer.readBigUInt64LE(offset); offset += 8;
+    const version = buffer.readUInt32LE(offset); offset += 4;
+    const avFiles = buffer.readUInt32LE(offset); offset += 4;
+    const nextFileRecNo = buffer.readUInt32LE(offset); offset += 4;
+    const lastFileRecNo = buffer.readUInt32LE(offset); offset += 4;
+    const curFileRec = buffer.subarray(offset, offset + 1176); offset += 1176;
+    const unknown = buffer.subarray(offset, offset + 76); offset += 76;
+    const checksum = buffer.readUInt32LE(offset); offset += 4;
+    return { modifyTimes, version, avFiles, nextFileRecNo, lastFileRecNo, curFileRec, unknown, checksum };
   }
 
-  getSegments({ fromTime = null, toTime = null, fromUnixtime = null, toUnixtime = null } = {}) {
-    if (fromUnixtime) fromTime = DateTime.fromSeconds(fromUnixtime).toUTC();
-    if (toUnixtime) toTime = DateTime.fromSeconds(toUnixtime).toUTC();
-    if (fromTime) fromUnixtime = Math.floor(fromTime.toSeconds());
-    if (toTime) toUnixtime = Math.floor(toTime.toSeconds());
-
-    return this.indexType === 'bin'
-      ? this.getSegmentsBIN(fromTime, toTime)
-      : this.getSegmentsSQL(fromUnixtime, toUnixtime);
+  parseSegment(buffer) {
+    let offsetInSegment = 0;
+    const type = buffer.readUint8(offsetInSegment); offsetInSegment += 1;
+    const status = buffer.readUint8(offsetInSegment); offsetInSegment += 1;
+    const resA = buffer.readUint16LE(offsetInSegment); offsetInSegment += 2;
+    const resolution = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const startTime = buffer.readBigUInt64LE(offsetInSegment); offsetInSegment += 8;
+    const endTime = buffer.readBigUInt64LE(offsetInSegment); offsetInSegment += 8;
+    const firstKeyFrame_absTime = buffer.readBigUInt64LE(offsetInSegment); offsetInSegment += 8;
+    const firstKeyFrame_stdTime = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const lastFrame_stdTime = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const startOffset = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const endOffset = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const resB = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const infoNum = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const infoTypes = buffer.readBigUInt64LE(offsetInSegment); offsetInSegment += 8;
+    const infoStartTime = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const infoEndTime = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const infoStartOffset = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    const infoEndOffset = buffer.readUInt32LE(offsetInSegment); offsetInSegment += 4;
+    return {
+      type, status, resA, resolution, startTime, endTime, firstKeyFrame_absTime, firstKeyFrame_stdTime, lastFrame_stdTime, startOffset, endOffset, resB, infoNum, infoTypes, infoStartTime, infoEndTime, infoStartOffset, infoEndOffset,
+    };
   }
 
-  getSegmentsSQL(fromUnixtime = null, toUnixtime = null) {
-    this.segments = [];
-    let where = 'record_type != 0';
-    if (fromUnixtime) where += ` AND start_time_tv_sec >= ${fromUnixtime}`;
-    if (toUnixtime) where += ` AND start_time_tv_sec <= ${toUnixtime}`;
+  processSegments() {
+    const buffer = fs.readFileSync(this.indexPath);
+    const startOffset = this.headerBufferLength + this.header.avFiles * this.file_len;
+    let offset = startOffset;
 
-    const statement = `SELECT file_no, start_offset, end_offset, start_time_tv_sec, end_time_tv_sec FROM record_segment_idx_tb WHERE ${where} ORDER BY start_time_tv_sec`;
+    for (let sourceFileIndex = 0; sourceFileIndex < this.header.avFiles; sourceFileIndex++) {
+      for (let sourceFileSegmentIndex = 0; sourceFileSegmentIndex < this.maxSegmentsInSourceFile; sourceFileSegmentIndex++) {
+        if (offset + this.segmentBufferLength > buffer.length) break;
 
-    for (let indexFileNum = 0; indexFileNum < this.info.DataDirs; indexFileNum++) {
-      const fileName = this.getIndexPath(indexFileNum);
-      const db = new sqlite3.Database(fileName);
-      db.each(statement, (err, row) => {
-        if (err) throw err;
-        this.segments.push({
-          startOffset: row.start_offset,
-          endOffset: row.end_offset,
-          cust_indexFileNum: indexFileNum,
-          cust_startTime: DateTime.fromSeconds(row.start_time_tv_sec).toUTC(),
-          cust_endTime: DateTime.fromSeconds(row.end_time_tv_sec).toUTC(),
-          cust_duration: row.end_time_tv_sec - row.start_time_tv_sec,
-        });
-      });
-    }
-    return this.segments;
-  }
+        const segBuffer = buffer.subarray(offset, offset + this.segmentBufferLength);
+        const data = this.parseSegment(segBuffer);
+        offset += this.segmentBufferLength;
 
-  getSegmentsBIN(fromTime = null, toTime = null) {
-    this.segments = [];
-    const mask = 0xFFFFFFFF;
-    for (let indexFileNum = 0; indexFileNum < this.info.DataDirs; indexFileNum++) {
-      const fileName = this.getIndexPath(indexFileNum);
-      const buffer = fs.readFileSync(fileName);
-      const offset = this.header_len + this.header.avFiles * this.file_len;
-      let pos = offset;
-      for (let fileNum = 0; fileNum < this.header.avFiles; fileNum++) {
-        for (let i = 0; i < this.maxSegments; i++) {
-          const seg = buffer.slice(pos, pos + this.segment_len);
-          const startTime = seg.readBigUInt64LE(8) & BigInt(mask);
-          const endTime = seg.readBigUInt64LE(16) & BigInt(mask);
+        if (data.endTime === 0n) continue;
 
-          if (endTime !== BigInt(0)) {
-            const start = DateTime.fromSeconds(Number(startTime)).toUTC();
-            const end = DateTime.fromSeconds(Number(endTime)).toUTC();
-            if (
-              (!fromTime && !toTime) ||
-              (fromTime && start > fromTime && !toTime) ||
-              (!fromTime && toTime && start < toTime) ||
-              (fromTime && toTime && start > fromTime && start < toTime)
-            ) {
-              this.segments.push({
-                cust_indexFileNum: indexFileNum,
-                cust_fileNum: fileNum,
-                startOffset: seg.readUInt32LE(32),
-                endOffset: seg.readUInt32LE(36),
-                cust_startTime: start,
-                cust_endTime: end,
-                cust_duration: end.diff(start, 'seconds').seconds,
-                cust_filePath: `${this.cameradir}/datadir${indexFileNum}/hiv${fileNum.toString().padStart(5, '0')}.${this.asktype === 'video' ? 'mp4' : 'pic'}`,
-              });
-            }
-          }
-          pos += this.segment_len;
-        }
+        const dateMask = 0x00000000ffffffffn;
+        const startTimeTimestamp = Number(data.startTime & dateMask);
+        const endTimeTimestamp = Number(data.endTime & dateMask);
+        const firstKeyFrame_absTimeTimestamp = Number(data.firstKeyFrame_absTime & dateMask);
+
+        const timestampToString = (timestamp) => {
+          const date = new Date(timestamp * 1000);
+          const pad = (num) => String(num).padStart(2, '0');
+          return `${date.getUTCFullYear()}-${pad(date.getUTCMonth() + 1 /* Months are zero-based */)}-${pad(date.getUTCDate())} ${pad(date.getUTCHours())}:${pad(date.getUTCMinutes())}:${pad(date.getUTCSeconds())}`;
+        };
+
+        const sourceFileIndexString = String(sourceFileIndex).padStart(5, '0');
+        const sourceFileSegmentIndexString = String(sourceFileSegmentIndex).padStart(3, '0');
+        const sourceFileName = `hiv${sourceFileIndexString}.mp4`;
+        const segment = {
+          data,
+          startTimeTimestamp,
+          startTime: timestampToString(startTimeTimestamp),
+          endTimeTimestamp,
+          endTime: timestampToString(endTimeTimestamp),
+          firstKeyFrame_absTimeTimestamp,
+          firstKeyFrame_absTime: timestampToString(firstKeyFrame_absTimeTimestamp),
+          sourceFileIndex,
+          sourceFileIndexString,
+          sourceFileName,
+          sourceFileSegmentIndex,
+          sourceFileSegmentIndexString,
+        };
+        this.segments.push(segment);
       }
     }
-    return this.segments;
+    this.segments.sort((a, b) => a.startTimeTimestamp - b.startTimeTimestamp);
+    console.log(`Found ${this.segments.length} recordings`)
   }
 
-  extractSegmentMP4(indx, cachePath = '/tmp', filename = null, resolution = null, debug = false, replace = true) {
-    const seg = this.segments[indx];
-    const h264File = `${cachePath}/hik_datadir${seg.cust_indexFileNum}_${seg.startOffset}_${seg.endOffset}.h264`;
-    const mp4File = filename || h264File.replace('.h264', '.mp4');
-
-    if (fs.existsSync(mp4File) && replace) fs.unlinkSync(mp4File);
-    if (!fs.existsSync(mp4File) || replace) {
-      const buffer = fs.readFileSync(seg.cust_filePath);
-      fs.writeFileSync(h264File, buffer.slice(seg.startOffset, seg.endOffset));
-
-      const cmd = resolution
-        ? `ffmpeg -i ${h264File} -threads auto -s ${resolution} -c:a none ${mp4File}`
-        : `ffmpeg -i ${h264File} -threads auto -c:v copy -c:a none ${mp4File}`;
-
-      execSync(cmd, { stdio: debug ? 'inherit' : 'ignore' });
-      fs.unlinkSync(h264File);
-    }
-    return mp4File;
+  readFileRange(path, offset, length) {
+    const fd = fs.openSync(path, 'r');
+    const buffer = Buffer.alloc(length);
+    fs.readSync(fd, buffer, 0, length, offset);
+    fs.closeSync(fd);
+    return buffer;
   }
 
-  extractSegmentJPG(indx, cachePath = '/tmp', filename = null, resolution = null, debug = false, replace = true, position = null) {
-    const seg = this.segments[indx];
-    const h264File = `${cachePath}/hik_datadir${seg.cust_indexFileNum}_${seg.startOffset}_${seg.endOffset}.h264`;
-    const jpgFile = filename || h264File.replace('.h264', '.jpg');
+  extractSegmentMP4(index, targetPath = './extracted', replace = true) {
+    console.log(`Processing ${index} recording`);
+    fs.mkdirSync(targetPath, { recursive: true });
+    const seg = this.segments[index];
+    const sanitizeDatePath = (dateString) => dateString.replace(/:/g, '-');
+    const h265File = path.join(
+      targetPath,
+      `${sanitizeDatePath(seg.startTime)} - ${sanitizeDatePath(seg.endTime)} (${seg.sourceFileIndexString}-${seg.sourceFileSegmentIndexString}).h265`,
+    );
+    const movFile = h265File.replace('.h265', '.mov');
 
-    if (fs.existsSync(jpgFile) && replace) fs.unlinkSync(jpgFile);
-    if (!fs.existsSync(jpgFile)) {
-      const buffer = fs.readFileSync(seg.cust_filePath);
-      fs.writeFileSync(h264File, buffer.slice(seg.startOffset, seg.endOffset));
+    if (fs.existsSync(movFile) && replace) fs.unlinkSync(movFile);
+    if (!fs.existsSync(movFile) || replace) {
+      const buffer = this.readFileRange(path.join(this.cameraDir, seg.sourceFileName), seg.data.startOffset, seg.data.endOffset);
+      fs.writeFileSync(h265File, buffer);
 
-      let jpgPos = position || Math.min(59, Math.floor(seg.cust_duration / 2));
-      const cmd = resolution
-        ? `ffmpeg -ss 00:00:${jpgPos} -i ${h264File} -vframes 1 -s ${resolution} ${jpgFile}`
-        : `ffmpeg -ss 00:00:${jpgPos} -i ${h264File} -vframes 1 ${jpgFile}`;
+      const macOSQuickTimePlayerSupport = '-tag:v hvc1';
+      const cmd = `ffmpeg -i "${h265File}" -acodec copy -vcodec copy ${macOSQuickTimePlayerSupport} -f mov "${movFile}"`;
+      const ffmpegLogPath = path.join('.', 'ffmpeg.log');
+      const ffmpegLogFileHandle = fs.openSync(ffmpegLogPath, 'a');
 
-      execSync(cmd, { stdio: debug ? 'inherit' : 'ignore' });
-      fs.unlinkSync(h264File);
+      try {
+        fs.writeSync(ffmpegLogFileHandle, `${cmd}\n`);
+        console.log('Running ffmpeg...');
+        execSync(cmd, { stdio: ['ignore', ffmpegLogFileHandle, ffmpegLogFileHandle] });
+        fs.unlinkSync(h265File);
+      } catch (err) {
+        console.error('Error caught while running ffmpeg. See ffmpeg.log');
+      }
+      fs.closeSync(ffmpegLogFileHandle);
     }
-    return jpgFile;
+    return movFile;
+  }
+
+  extractAllSegments(targetPath = './extracted', replace = true) {
+    this.segments.forEach((segment, index) => {
+      this.extractSegmentMP4(index, targetPath, replace);
+    });
   }
 }
 
